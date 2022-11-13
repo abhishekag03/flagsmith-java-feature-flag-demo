8758150a6a1f959967410b0d13663bd3706942d0
Abhishek Agarwal <abhishek16126@iiitd.ac.in>
2022-11-13 18:19:39 +0530

    add flagsmith configuration code

--- src/main/java/org/flagsmith/Main.java
+++ src/main/java/org/flagsmith/Main.java
@@ -1,31 +1,32 @@
 package org.flagsmith;
 
+import com.flagsmith.FlagsmithClient;
 import com.google.gson.Gson;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Properties;
 
 public class Main {
 
     static List<Book> books = new ArrayList<>();
+    static FlagsmithClient fsClient;
+
 
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        populateDummyData();
+        fsClient = getFlagsmithClient();
         server.createContext("/books", new MyHandler());
         server.setExecutor(null); // creates a default executor
-        populateDummyData();
         server.start();
     }
-
     private static void populateDummyData() {
         books.add(new Book("1", "Harry Potter", "J.K. Rowling", "20.99"));
         books.add(new Book("2", "War and Peace", "Leo Tolstoy", "26.99"));
@@ -68,5 +69,29 @@ public class Main {
             Gson gson = new Gson();
             return gson.fromJson(query, Book.class);
         }
+
+    }
+    private static FlagsmithClient getFlagsmithClient() {
+        String apiKey = readFlagsmithApiKey();
+        return FlagsmithClient
+                .newBuilder()
+                .setApiKey(apiKey)
+                .build();
+    }
+    private static String readFlagsmithApiKey() {
+        Properties prop = new Properties();
+        String propFileName = "config.properties";
+
+        InputStream inputStream = ClassLoader.getSystemResourceAsStream(propFileName);
+        if (inputStream != null) {
+            try {
+                prop.load(inputStream);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        } else {
+            throw new RuntimeException("config file input stream is null");
+        }
+        return prop.getProperty("apiKey");
     }
 }
