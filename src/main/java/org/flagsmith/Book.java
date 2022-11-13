6848a7c9bdde6dcc56f2cb8695518c6ec7f580bd
Abhishek Agarwal <abhishek16126@iiitd.ac.in>
2022-11-13 17:55:33 +0530

    populate mock data

--- src/main/java/org/flagsmith/Book.java
+++ src/main/java/org/flagsmith/Book.java
@@ -1,2 +1,47 @@
-package org.flagsmith;public class Book {
+package org.flagsmith;
+
+public class Book {
+    String id;
+    String title;
+    String author;
+    String price;
+
+    public Book(String id, String title, String author, String price) {
+        this.id = id;
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getPrice() {
+        return price;
+    }
+
+    public void setPrice(String price) {
+        this.price = price;
+    }
 }
--- src/main/java/org/flagsmith/Main.java
+++ src/main/java/org/flagsmith/Main.java
@@ -7,15 +7,27 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
+
+    static List<Book> books = new ArrayList<>();
+
     public static void main(String[] args) throws Exception {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
-        server.createContext("/test", new MyHandler());
+        server.createContext("/books", new MyHandler());
         server.setExecutor(null); // creates a default executor
-
+        populateDummyData();
         server.start();
     }
+
+    private static void populateDummyData() {
+        books.add(new Book("1", "Harry Potter", "J.K. Rowling", "20.99"));
+        books.add(new Book("2", "War and Peace", "Leo Tolstoy", "26.99"));
+        books.add(new Book("3", "The Kite Runner", "Khaled Hosseini", "30.99"));
+    }
+
 
     static class MyHandler implements HttpHandler {
         @Override
